bf3fbbb467ed0cacc3c5fb3d8925ed5efbf63f67
Hugo Lindström <huggepugge1@gmail.com>
2024-11-12 12:16:04 +0100

    .

---
+++ lua/test-script-highlighter/init.lua
@@ -0,0 +1,13 @@
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.sdjot = {
+	install_info = {
+		-- Change this url to your grammar
+		url = "https://github.com/Huggepugge1/test-script-parser",
+		-- If you use an external scanner it needs to be included here
+		files = { "src/parser.c", "src/scanner.c" },
+		generate_reqires_npm = false,
+		requires_generate_from_grammar = false,
+	},
+	-- The filetype you want it registered as
+	filetype = "tesc",
+}
