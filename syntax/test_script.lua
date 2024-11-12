0d83474f346201d0bb586c10eecbee771dbddfbe
Hugo Lindström <huggepugge1@gmail.com>
2024-11-12 10:59:05 +0100

    order

--- syntax/test_script.lua
+++ syntax/test_script.lua
@@ -7,27 +7,28 @@ vim.api.nvim_command("syntax clear")
 -- Keywords
 vim.api.nvim_command("syntax keyword test_scriptKeyword let const for in")
 
+-- Identifiers
+vim.api.nvim_command("syntax match test_scriptIdentifier /\\w[\\w\\d]\\+/")
+
+-- Operators
+vim.api.nvim_command("syntax match test_scriptOperator /[+\\-*\\/=]\\+/")
+
 -- String
 vim.api.nvim_command('syntax region test_scriptString start=/"/ end=/"/ contains=test_scriptEscape')
 
 -- Numbers
 vim.api.nvim_command("syntax match test_scriptNumber /\\d\\+/")
 
--- Identifiers
-vim.api.nvim_command("syntax match test_scriptIdentifier /\\w[\\w\\d]\\+/")
-
 -- Comments
 vim.api.nvim_command("syntax match test_scriptComment /\\/.*$/")
 
--- Operators
-vim.api.nvim_command("syntax match test_scriptOperator /[+\\-*\\/=]\\+/")
-
 -- Link groups to highlight groups
 vim.api.nvim_command("highlight default link test_scriptKeyword Keyword")
+vim.api.nvim_command("highlight default link test_scriptIdentifier Identifier")
+vim.api.nvim_command("highlight default link test_scriptOperator Operator")
+
 vim.api.nvim_command("highlight default link test_scriptString String")
 vim.api.nvim_command("highlight default link test_scriptNumber Number")
-vim.api.nvim_command("highlight default link test_scriptIdentifier Identifier")
 vim.api.nvim_command("highlight default link test_scriptComment Comment")
-vim.api.nvim_command("highlight default link test_scriptOperator Operator")
 
 return M
