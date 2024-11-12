b08181ea2f91e78caaabdfe5aa2e8b258350b906
Hugo Lindström <huggepugge1@gmail.com>
2024-11-12 13:29:01 +0100

    .

--- queries/test_script/highlights.scm
+++ queries/test_script/highlights.scm
@@ -1,12 +1,20 @@
 ; Keywords
+(assignment_keyword) @keyword
 
 ; Tests
 (test) @function
+
+; Variables
+(identifier) @variable
 
 ; Literal
 (number) @number
 (string) @string
 (regex) @string.regexp
+
+; Operators
+(colon) @operator
+(assignment_operator) @operator
 
 ; Comment
 (comment) @comment
